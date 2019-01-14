7cd80674197f953caf1e18d86def236e8e93f233
Cassandra Romero <romero.cassandra1@gmail.com>
2019-01-14 11:55:09 -0700

    committed PHP index file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+		<meta charset="UTF-8"/>
+	<title>Basic Demo of Project Creation</title>
+		</head>
+		<body>
+			<h1>Deep Dive Full Stack Cohort 23</h1>
+		</body>
+	</html>
